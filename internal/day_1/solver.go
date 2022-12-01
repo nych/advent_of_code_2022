c8c762d2cd236c75e5dfdbe8051923404ece3d40
Christian Nydegger <nydegger.ch@gmail.com>
2022-12-01 18:16:46 +0100

    solve day 1
    Signed-off-by: Christian Nydegger <nydegger.ch@gmail.com>

--- internal/day_1/solver.go
+++ internal/day_1/solver.go
@@ -3,8 +3,116 @@ package day1
 import (
 	"bufio"
 	"fmt"
+	"io"
+	"sort"
+	"strconv"
 )
 
-func Solve(s *bufio.Scanner) (string, error) {
-	return "", fmt.Errorf("not yet implemented")
+func SolvePartOne(reader io.Reader) (string, error) {
+	if reader == nil {
+		return "", fmt.Errorf("reader arg cannot be nil")
+	}
+
+	elves, err := loadElves(reader)
+	if err != nil {
+		return "", fmt.Errorf("loading elves failed, %v", err)
+	}
+
+	sort.Slice(elves, func(i, j int) bool {
+		return elves[i].carryingCalories() > elves[j].carryingCalories()
+	})
+
+	return strconv.Itoa(elves[0].carryingCalories()), nil
+}
+
+func SolvePartTwo(reader io.Reader) (string, error) {
+	if reader == nil {
+		return "", fmt.Errorf("reader arg cannot be nil")
+	}
+
+	elves, err := loadElves(reader)
+	if err != nil {
+		return "", fmt.Errorf("loading elves failed, %v", err)
+	}
+
+	sort.Slice(elves, func(i, j int) bool {
+		return elves[i].carryingCalories() > elves[j].carryingCalories()
+	})
+
+	sum := 0
+	for i := range elves {
+		if i > 2 {
+			break
+		}
+		sum += elves[i].carryingCalories()
+	}
+
+	return strconv.Itoa(sum), nil
+}
+
+func loadElves(reader io.Reader) ([]elf, error) {
+	s := bufio.NewScanner(reader)
+
+	elves := make([]elf, 0)
+	elf := createElf(0)
+
+	for s.Scan() {
+		l := s.Text()
+
+		if len(l) == 0 {
+			elves = append(elves, elf)
+			elf = createElf(elf.id + 1)
+			continue
+		}
+
+		calories, err := strconv.Atoi(l)
+		if err != nil {
+			return nil, err
+		}
+
+		i := item{
+			id:       calories,
+			calories: calories,
+		}
+
+		if _, ok := elf.items[i]; !ok {
+			elf.items[i] = 0
+		}
+		elf.items[i]++
+	}
+
+	if s.Err() != nil {
+		return nil, s.Err()
+	}
+
+	if len(elves) == 0 {
+		elves = append(elves, elf)
+	}
+
+	return elves, nil
+}
+
+type elf struct {
+	id    int
+	items map[item]int
+}
+
+type item struct {
+	id       int
+	calories int
+}
+
+func createElf(id int) elf {
+	return elf{
+		id:    id,
+		items: make(map[item]int),
+	}
+}
+
+func (e elf) carryingCalories() int {
+	var r int = 0
+	for i, n := range e.items {
+		r += i.calories * n
+	}
+	return r
 }
--- internal/day_1/solver_test.go
+++ internal/day_1/solver_test.go
@@ -1,14 +1,14 @@
 package day1
 
 import (
-	"bufio"
+	"io"
 	"strings"
 	"testing"
 )
 
-func TestSolve(t *testing.T) {
+func TestSolvePartOne(t *testing.T) {
 	type args struct {
-		s *bufio.Scanner
+		r io.Reader
 	}
 	tests := []struct {
 		name    string
@@ -19,32 +19,104 @@ func TestSolve(t *testing.T) {
 		{
 			name: "example input",
 			args: args{
-				bufio.NewScanner(strings.NewReader(
-					"1000" +
-						"2000" +
-						"3000\n" +
-						"4000\n" +
-						"5000" +
-						"6000\n" +
-						"7000" +
-						"8000" +
-						"9000\n" +
-						"10000",
-				)),
+				strings.NewReader(
+					"1000\n" +
+						"2000\n" +
+						"3000\n\n" +
+						"4000\n\n" +
+						"5000\n" +
+						"6000\n\n" +
+						"7000\n" +
+						"8000\n" +
+						"9000\n\n" +
+						"10000\n\n",
+				),
 			},
 			want:    "24000",
 			wantErr: false,
 		},
+		{
+			name: "one elf only",
+			args: args{
+				strings.NewReader("1000\n"),
+			},
+			want:    "1000",
+			wantErr: false,
+		},
+		{
+			name: "multiple empty lines",
+			args: args{
+				strings.NewReader("\n\n\n\n\n"),
+			},
+			want:    "0",
+			wantErr: false,
+		},
+		{
+			name: "no input",
+			args: args{
+				strings.NewReader(""),
+			},
+			want:    "0",
+			wantErr: false,
+		},
+		{
+			name:    "reader is nil",
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := Solve(tt.args.s)
+			got, err := SolvePartOne(tt.args.r)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Solve() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if got != tt.want {
 				t.Errorf("Solve() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSolvePartTwo(t *testing.T) {
+	type args struct {
+		reader io.Reader
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "example input",
+			args: args{
+				strings.NewReader(
+					"1000\n" +
+						"2000\n" +
+						"3000\n\n" +
+						"4000\n\n" +
+						"5000\n" +
+						"6000\n\n" +
+						"7000\n" +
+						"8000\n" +
+						"9000\n\n" +
+						"10000\n\n",
+				),
+			},
+			want:    "45000",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := SolvePartTwo(tt.args.reader)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SolvePartTwo() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("SolvePartTwo() = %v, want %v", got, tt.want)
 			}
 		})
 	}
